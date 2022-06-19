83e7f5ad9dae57a2c0261e8a8f229d24b8048662
omer liberman <oliberman@dropbox.com>
2022-06-19 10:22:21 +0300

    add update endpoint

--- api/attendies.js
+++ api/attendies.js
@@ -29,11 +29,33 @@ router.get('/:id', async (req, res) => {
     try {
         const attendy = await Attender.findById(id);
         if (attendy) {
-            res.status(200).send(attendy.name);
+            res.status(200).json({
+                name: attendy.name,
+                attendies: attendy.attendies,
+            });
         }
     } catch (err) {
-        res.status(401).send('Unrecognized');
+        res.status(401).json({message: 'Unrecognized'});
     }
+});
+
+router.put('/', async (req, res) => {
+    const {id, name, phone, status, attendies, recognized} = req.body;
+    if (!id || !name || !phone || !status) {
+        res.status(500);
+    };
+    try {
+        await Attender.findOneAndUpdate({_id: id}, {
+            name,
+            phone,
+            status,
+            attendies,
+            recognized,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+    res.status(200).send('updated successfully');
 });
 
 module.exports = router;
